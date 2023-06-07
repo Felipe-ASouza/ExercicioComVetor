a05dda8a3dd31c387c3b5570b2d80cc222fe7329
Felipe Alexandre <felipe.alexandre3388@gmail.com>
2023-06-06 23:54:45 -0300

    Exercício utilizando Vetores para calcular o preço médio dos produtos informados

---
+++ Vetores2/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace Vetores2
+{
+    public class Produto
+    {
+        public string Nome { get; set; }
+        public double Preco { get; set; }
+        
+        
+    }
+}
---
+++ Vetores2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace Vetores2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de produtos diferentes: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Produto[] vect = new Produto[n];
+            for (int i = 0; i < n; i++)
+            {
+                string nome = Console.ReadLine();
+                double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Produto { Nome = nome, Preco = preco };
+            }
+
+            double soma = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                soma += vect[i].Preco;
+            }
+
+            double media = soma / n;
+            Console.WriteLine("Preco médio = " + media.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
